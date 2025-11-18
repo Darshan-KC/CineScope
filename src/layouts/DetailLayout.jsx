d93d76a0643893b04ab1cfe119c6cb57def2fe56
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-11-18 15:56:51 +0545

    feat: app routes

---
+++ src/layouts/DetailLayout.jsx
@@ -0,0 +1,5 @@
+export default function DetailLayout(){
+    return (
+        <></>
+    )
+}
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,13 +4,29 @@ import Homepage from "../pages/HomePage";
 import MovieDetailPage from "../pages/MovieDetailPage";
 import SearchPage from "../pages/SearchPage";
 import NotFoundPage from "../pages/NotFoundPage";
+import MainLayout from "../layouts/MainLayout";
+import DetailLayout from "../layouts/DetailLayout";
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/" element={<HomePage />} />
             <Route path="/movie/:id" element={<MovieDetailPage />} />
-            <Route path="/search" element={<SearchPage />} />
+            <Route path="/search" element={<SearchPage />} /> */}
+
+            {/* Main Layout Pages */}
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/favorites" element={<FavoritesPage />} />
+            </Route>
+
+            {/* Detail Layout */}
+            <Route element={<DetailLayout />}>
+                <Route path="/movie/:id" element={<MovieDetailPage />} />
+            </Route>
+
+            {/* 404 */}
             <Route path="" element={<NotFoundPage />} />
         </Routes>
     )
