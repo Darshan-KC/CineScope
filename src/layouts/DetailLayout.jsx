0c6dab4481b3850c125f51c716cf096e79060760
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-11-19 13:55:56 +0545

    feat: Detail layout

--- src/layouts/DetailLayout.jsx
+++ src/layouts/DetailLayout.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/common/Navbar";
+
 export default function DetailLayout(){
     return (
-        <></>
+        <>
+            <Navbar />
+            <div className="max-w-5xl mx-auto">
+                <Outlet />
+            </div>
+        </>
     )
 }
