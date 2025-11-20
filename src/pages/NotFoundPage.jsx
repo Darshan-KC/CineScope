2d9b7bb59c40ee58510ec55f10872e9db758ae80
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-11-20 20:31:09 +0545

    feat: Page not found

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage(){
+    return (
+        <div className="text-center text-white py-32">
+            <h1 className="text-5xl font-bold mb-4">404</h1>
+            <p className="text-gray-400">Page Not Found</p>
+        </div>
+    );
+}
