8243ee6d4d24f184e76d0bceeabc11a29b2f37b0
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-11-24 21:16:19 +0545

    feat: movie detail page

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,5 +1,33 @@
-export default function(){
+import { useParams } from "react-router"
+import Loader from "../components/common/Loader";
+import ErrorMessage from "../components/common/ErrorMessage";
+import { getPosterUrl } from "../utils/getPosterUrl";
+
+export default function () {
+    const { id } = useParams();
+    const { data: movie, loading, error } = {};
+
+    if (loading) return <Loader />;
+    if (error) return <ErrorMessage message="Movie not found." />;
+
     return (
-        <></>
-    )
+        <div className="p-6 text-white">
+            <div className="flex flex-col md:flex-row gap-8">
+                <img src={getPosterUrl(movie.poster_path)}
+                    className="w-72 rounded-xl shadow-lg"
+                />
+                <div>
+                    <h1 className="text-4xl font-bold mb-2">{movie.title}</h1>
+                    <p className="text-gray-300 mb-4">{movie.overview}</p>
+
+                    <p className="text-sm text-gray-400">
+                        Release Date: {movie.release_date}
+                    </p>
+                    <p className="text-sm text-gray-400">
+                        Rating: {movie.vote_average.toFixed(1)}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
