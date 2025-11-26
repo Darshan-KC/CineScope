af2cdcae079529753883a825ccd5044c63d3a9f7
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-11-26 21:11:34 +0545

    feat: search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,5 +1,18 @@
-export default function SearchPage(){
+import { useSearchParams } from "react-router";
+
+export default function SearchPage() {
+    const [params] = useSearchParams();
+    const query = params.get("query");
+
+    const { data: data, loading, error } = {};
     return (
-        <div></div>
+        <div className="p-6">
+            <h1 className="text-xl text-white font-semibold mb-4">
+                Search Results for: <span className="text-yellow-400">{query}</span>
+            </h1>
+
+            <MovieGrid movies={movies} loading={loading} error={error} />
+
+        </div>
     );
 }
