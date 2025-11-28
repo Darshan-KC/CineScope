a0131fb272a5d4ddbc16bdeaabfdb77114981afd
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-11-28 21:41:45 +0545

    feat: Theme context

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { createContext } from "react";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") || "light"
+    );
 
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        localStorage.setItem("theme", newTheme);
+    };
+
+    // Add class to HTML for Tailwind dark mode
+    useEffect(()=> {
+        if(theme == "dark"){
+            document.documentElement.classList.add("dark");
+        }else{
+            document.documentElement.classList.remove("dark");
+        }
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
 };
