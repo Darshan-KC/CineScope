16b810fcb88bfea5204e2d9b0e47c82de8a8dc2c
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-11-30 21:42:31 +0545

    feat: add favorite in favorites context provider

--- src/contexts/FavoritesContext.jsx
+++ src/contexts/FavoritesContext.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import { createContext } from "react";
 
 export const FavoritesContext = createContext();
 
 export const FavoritesProvider = ({children}) => {
+    const [favorites, setFavorites] = useState(
+        JSON.parse(localStorage.getItem("favorites")) || []
+    );
+
+    const addFavorite = (movie) => {
+        if(!favorites.find((m) => m.id === movie.id)){
+            const update = [...favorites, movie];
+            setFavorites(update);
+            localStorage.setItem("favorites", JSON.stringify(update));
+        }
+    };
 
     return (
         <FavoritesContext.Provider >
