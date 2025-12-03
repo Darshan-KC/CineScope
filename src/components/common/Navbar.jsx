c84d2fc129a229bf9fcac0d07f4d46214362e61f
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-12-03 21:47:34 +0545

    feat: navbar context

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import useDebounce from "../../hooks/useDebounce";
+import { useContext } from "react";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { Sun, Moon } from "lucide-react"; // if using icons
 
 
 export default function Navbar() {
+    const { theme, toggleTheme } = useContext(ThemeContext);
     const [query, setQuery] = useState("");
     const navigate = useNavigate();
     const debounceQuery = useDebounce(query, 500);
@@ -16,13 +20,16 @@ export default function Navbar() {
     return (
         <nav className="bg-gray-900 text-white px-6 py-3 flex items-center justify-between shadow-md">
             <Link to="/" className="text-2xl font-bold text-yellow-400 tracking-wide">
-            CineScope
+                CineScope
             </Link>
 
             <div className="relative w-64">
-                <input type="text" placeholder="Search movies..." value={query} onChange={(e)=>setQuery(e.target.value)} 
-                className="w-full px-4 py-2 bg-gray-800 rounded-lg text-gray-200 focus:outline-none focus:ring-2 focus:ring-yellow-400"/>
+                <input type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)}
+                    className="w-full px-4 py-2 bg-gray-800 rounded-lg text-gray-200 focus:outline-none focus:ring-2 focus:ring-yellow-400" />
                 <button className="absolute right-3 top-2.5 text-gray-400" />
+                <button onClick={toggleTheme}>
+                    {theme === "light" ? <Moon /> : <Sun />}
+                </button>
             </div>
         </nav>
     )
