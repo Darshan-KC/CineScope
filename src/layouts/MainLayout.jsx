bc44c6452dc4a05c801853af62891ae41e12528a
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-12-06 20:12:11 +0545

    fix: outlets

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
+import { Outlet } from "react-router";
 
 export default function MainLayout({ children }) {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <main className="flex-grow">{children}</main>
+            <main className="flex-grow">
+                {/* {children} */}
+                <Outlet />
+                </main>
             <Footer />
         </div>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     return (
         <>
         <div className="p-6">
-            <h1 className="text-2xl font-bold text-white mb-4">Trending Movies</h1>
+            <h1 className="text-2xl font-bold text-black mb-4">Trending Movies</h1>
             {/* <MovieGrid movies={movies} loading={loading} error={error} /> */}
             <MovieGrid movies={movies} />
         </div>
