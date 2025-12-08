f377c4c269b4badc3fe856560435478b8df6cd2f
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-12-08 19:26:25 +0545

    feat: skeleton in movie grid

--- src/components/movies/MovieGrid.jsx
+++ src/components/movies/MovieGrid.jsx
@@ -1,9 +1,19 @@
 import ErrorMessage from "../common/ErrorMessage";
 import Loader from "../common/Loader";
+import SkeletonCard from "../ui/SkeletonCard";
 import MovieCard from "./MovieCard";
 
 export default function MovieGrid({ movies, loading, error }) {
-    if (loading) return <Loader />;
+    // if (loading) return <Loader />;
+    if (loading) {
+        return (
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+                {[...Array(8)].map((_, index) => (
+                    <SkeletonCard key={index} />
+                ))}
+            </div>
+        );
+    }
     if (error) return <ErrorMessage message="Failed to load movies." />;
     if (!movies?.length) return <ErrorMessage message="No movies found." />;
 
