d609bcf792014c0221ec553f657147b650e26a20
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-12-09 18:43:58 +0545

    feat: movie card

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -4,24 +4,29 @@ import RatingBadge from "./RatingBadge";
 
 export default function MovieCard({ movie }) {
   return (
-    <Link
-      to={`/movie/${movie.id}`}
-      className="bg-gray-800 rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition transform hover:-translate-y-1 duration-300"
+    <div
+      className="relative transition-all duration-300 hover:scale-105 hover:shadow-lg opacity-0 animate-fadeIn"
     >
-      <img
-        src={getPosterUrl(movie.poster_path)}
-        alt={movie.title}
-        className="w-full h-80 object-cover"
-      />
-      <div className="p-4">
-        <h3 className="text-lg font-semibold text-white mb-2">
-          {truncateText(movie.title, 30)}
-        </h3>
-        <div className="flex justify-between items-center text-sm text-gray-400">
-          <span>{movie.release_date?.slice(0, 4) || "N/A"}</span>
-          <RatingBadge rating={movie.vote_average} />
+
+      <Link
+        to={`/movie/${movie.id}`}
+        className="bg-gray-800 rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition transform hover:-translate-y-1 duration-300"
+      >
+        <img
+          src={getPosterUrl(movie.poster_path)}
+          alt={movie.title}
+          className="w-full h-80 object-cover"
+        />
+        <div className="p-4">
+          <h3 className="text-lg font-semibold text-white mb-2">
+            {truncateText(movie.title, 30)}
+          </h3>
+          <div className="flex justify-between items-center text-sm text-gray-400">
+            <span>{movie.release_date?.slice(0, 4) || "N/A"}</span>
+            <RatingBadge rating={movie.vote_average} />
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
