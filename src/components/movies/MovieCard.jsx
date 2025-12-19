39fb595c40b513790721fb707f09b78e597f161f
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-12-19 21:16:19 +0545

    fix: movie card card color

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -4,12 +4,12 @@ import RatingBadge from "./RatingBadge";
 
 export default function MovieCard({ movie }) {
   return (
-    <div className="relative transition-all duration-300 hover:scale-105 hover:shadow-lg animate-fadeIn">
+    <div className="relative bg-black dark:bg-gray-200 rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-lg animate-fadeIn">
       <Link
         to={`/movie/${movie.id}`}
         className="
           rounded-2xl overflow-hidden shadow-md transition transform hover:-translate-y-1 duration-300
-          bg-white text-gray-900
+          text-gray-900
           dark:bg-gray-800 dark:text-white
           hover:shadow-lg
         "
@@ -17,8 +17,8 @@ export default function MovieCard({ movie }) {
         <img
           src={getPosterUrl(movie.poster_path)}
           alt={movie.title}
-          className="w-full h-80 object-cover"
-          loading="lazy"
+          className="rounded-2xl w-full h-80 object-cover"
+          loading="lazy"x
         />
 
         <div className="p-4">
@@ -31,7 +31,7 @@ export default function MovieCard({ movie }) {
           </p>
 
           <div className="flex justify-between items-center text-sm">
-            <span className="text-gray-600 dark:text-gray-400">
+            <span className="text-gray-400 dark:text-gray-400">
               {movie.release_date?.slice(0, 4) || "N/A"}
             </span>
             <RatingBadge rating={movie.vote_average} />
